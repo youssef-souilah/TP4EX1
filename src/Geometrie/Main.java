8493afa5776a5af1dd206f04e7d8aab63c0139c5
Youssef Souilah <souilah.youssef.0@gmail.com>
2024-12-31 19:57:27 +0100

    ajouter le code de classe main

---
+++ src/Geometrie/Main.java
@@ -0,0 +1,29 @@
+package Geometrie;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Ex5
+		/*
+		 * 	Point p1=new Point(0,5);
+			Point p2=new Point(0,6);
+			Point3D p3=new Point3D(0,5,2);
+			Point3D p4=new Point3D(0,6,1);
+			PointNom3D p5=new PointNom3D(0,5,3,"p5");
+			PointNom3D p6=new PointNom3D(0,6,2,"p6");
+			p1.affCoord();
+			p2.affCoord();
+			p3.affCoord();
+			p4.affCoord();
+			p5.affCoord();
+			p6.affCoord();
+			
+			EX7 
+			oui on peut afficher les points dons une boucle
+		 * 
+		 */
+		
+	}
+
+}
